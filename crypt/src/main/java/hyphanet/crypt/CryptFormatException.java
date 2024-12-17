eee393bde63bdcb95b5fd30205eec1526c0739a4
Leumor <116955025+leumor@users.noreply.github.com>
2024-12-17 01:53:18 +0000

    Added CryptoKey, DSAGroup and DSAPrivateKey classes

---
+++ crypt/src/main/java/hyphanet/crypt/CryptFormatException.java
@@ -0,0 +1,46 @@
+package hyphanet.crypt;
+
+import java.io.IOException;
+import java.io.Serial;
+
+/**
+ * Exception thrown when there is an error in cryptographic data formatting. This exception is
+ * typically thrown when cryptographic data cannot be properly parsed, encoded, or decoded.
+ */
+public class CryptFormatException extends Exception {
+
+    @Serial
+    private static final long serialVersionUID = -796276279268900609L;
+
+    /**
+     * Constructs a new CryptFormatException with the specified detail message.
+     *
+     * @param message the detail message describing the cause of the exception
+     */
+    public CryptFormatException(String message) {
+        super(message);
+    }
+
+    /**
+     * Constructs a new CryptFormatException that wraps an IOException. The message from the
+     * IOException is used as this exception's detail message.
+     *
+     * @param e the IOException to be wrapped
+     */
+    public CryptFormatException(IOException e) {
+        super(e.getMessage());
+        initCause(e);
+    }
+
+    /**
+     * Constructs a new CryptFormatException with the specified detail message and cause.
+     *
+     * @param message the detail message describing the cause of the exception
+     * @param e       the underlying cause of this exception
+     */
+    public CryptFormatException(String message, Exception e) {
+        super(message);
+        initCause(e);
+    }
+
+}
---
+++ crypt/src/main/java/hyphanet/crypt/CryptoKey.java
@@ -0,0 +1,101 @@
+/* This code is part of Freenet. It is distributed under the GNU General
+ * Public License, version 2 (or at your option any later version). See
+ * http://www.gnu.org/ for further details of the GPL. */
+package hyphanet.crypt;
+
+import hyphanet.support.HexUtil;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigInteger;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+/**
+ * Represents a cryptographic key used for encryption and decryption operations. This class
+ * encapsulates the necessary information for a cryptographic key, including its value,
+ * algorithm, and format.
+ *
+ * <p>The key can be used in various cryptographic operations such as encryption, decryption,
+ * signing, and verification.</p>
+ *
+ * <p>This is an abstract class, concrete implementations will define specific key types.</p>
+ */
+public abstract class CryptoKey implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * Returns a string representation of the key type.
+     *
+     * @return A string representing the key type.
+     */
+    public abstract String keyType();
+
+    /**
+     * Generates a fingerprint for the key.
+     *
+     * @return A byte array containing the key's fingerprint.
+     */
+    public abstract byte[] fingerprint();
+
+    /**
+     * Converts the key to its byte representation.
+     *
+     * @return A byte array representing the key.
+     */
+    public abstract byte[] asBytes();
+
+    /**
+     * Provides a detailed string representation of the key.
+     *
+     * @return A string with detailed information about the key.
+     */
+    public abstract String toLongString();
+
+    /**
+     * Provides a string representation of the key.
+     * <p>
+     * This method returns a concise string representation of the key, including its type and a
+     * shortened fingerprint.
+     * </p>
+     *
+     * @return A string representation of the key in the format "keyType/shortFingerprint".
+     */
+    @Override
+    public String toString() {
+        StringBuilder b = new StringBuilder(keyType().length() + 1 + 4);
+        b.append(keyType()).append('/');
+        HexUtil.bytesToHexAppend(fingerprint(), 16, 4, b);
+        return b.toString();
+    }
+
+    /**
+     * Generates a fingerprint for the given BigInteger quantities.
+     * <p>
+     * This method uses SHA-1 to create a fingerprint from the provided BigInteger values. It's
+     * typically used by subclasses to implement their specific fingerprint() method.
+     * </p>
+     *
+     * @param quantities An array of BigInteger values to be included in the fingerprint.
+     *
+     * @return A byte array containing the SHA-1 hash of the provided quantities.
+     */
+    protected byte[] fingerprint(BigInteger[] quantities) {
+        try {
+            var shactx = MessageDigest.getInstance(Util.HashAlgorithm.SHA1.getAlgorithmName(),
+                                                   Util.MD_PROVIDERS.get(
+                                                       Util.HashAlgorithm.SHA1));
+            for (BigInteger quantity : quantities) {
+                byte[] mpi = Util.calcMPIBytes(quantity);
+                shactx.update(mpi, 0, mpi.length);
+            }
+            return shactx.digest();
+        } catch (NoSuchAlgorithmException e) {
+            // impossible
+            throw new IllegalStateException("This should never happen");
+        }
+
+    }
+
+}
