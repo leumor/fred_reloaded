af2b4144acb4aed29743a7a8a4b1e7e551e77215
Leumor <116955025+leumor@users.noreply.github.com>
2024-12-23 05:31:32 +0000

    Added NotPersistent interface

---
+++ support/src/main/java/hyphanet/support/io/bucket/NotPersistent.java
@@ -0,0 +1,8 @@
+package hyphanet.support.io.bucket;
+
+// A Bucket which does not support being stored to the database. E.g. SegmentedBCB.
+public interface NotPersistent extends Bucket {
+
+    // No methods
+
+}
