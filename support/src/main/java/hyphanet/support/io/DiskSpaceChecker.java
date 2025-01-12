33c292369308cc40a4925f9220f1ff1b0de0c41e
Leumor <116955025+leumor@users.noreply.github.com>
2025-01-12 10:38:55 +0000

    Improved JavaDoc for DiskSpaceChecker and PersistentFileTracker.

--- support/src/main/java/hyphanet/support/io/DiskSpaceChecker.java
+++ support/src/main/java/hyphanet/support/io/DiskSpaceChecker.java
@@ -1,19 +1,29 @@
 package hyphanet.support.io;
 
-import java.io.File;
+import java.nio.file.Path;
 
+/**
+ * Interface for checking available disk space before writing to files.
+ * <p>
+ * This interface provides functionality to verify if there is sufficient disk space available
+ * before performing write operations on files. It helps prevent disk space exhaustion and
+ * potential system failures.
+ * </p>
+ */
 public interface DiskSpaceChecker {
 
     /**
-     * Is there enough space to extend the file?
+     * Verifies if there is enough disk space available to extend a file.
      *
-     * @param file       The current filename
-     * @param toWrite    Length of the proposed write
-     * @param bufferSize The caller only checks disk space when the number of bytes written
-     *                   since the last check exceeds this value.
+     * @param path       The path of the file that needs to be extended
+     * @param toWrite    The number of bytes to be written
+     * @param bufferSize The threshold of accumulated bytes before performing a disk space
+     *                   check. The caller only checks disk space when the number of bytes
+     *                   written since the last check exceeds this value.
      *
-     * @return True if there is sufficient disk space
+     * @return {@code true} if there is sufficient disk space available, {@code false}
+     * otherwise
      */
-    boolean checkDiskSpace(File file, int toWrite, int bufferSize);
+    boolean checkDiskSpace(Path path, int toWrite, int bufferSize);
 
 }
--- support/src/main/java/hyphanet/support/io/PersistentFileTracker.java
+++ support/src/main/java/hyphanet/support/io/PersistentFileTracker.java
@@ -5,38 +5,62 @@ package hyphanet.support.io;
 
 import java.nio.file.Path;
 
+/**
+ * Interface for tracking and managing persistent files in a file system. Implements
+ * functionality for garbage collection, file registration, and delayed disposal of temporary
+ * files.
+ * <p>
+ * This tracker extends {@link DiskSpaceChecker} to ensure sufficient disk space is available
+ * for operations.
+ * </p>
+ */
 public interface PersistentFileTracker extends DiskSpaceChecker {
 
     /**
-     * While resuming, register a file with the garbage collector so that it doesn't get
-     * deleted when startup has finished and we cleanup the persistent-temp dir. We will only
-     * delete files which were present at startup and have not been claimed; new files are
-     * fine.
+     * Registers a file with the garbage collector during system resume.
+     * <p>
+     * This method prevents registered files from being deleted during startup cleanup of the
+     * persistent-temp directory. Only unregistered files that were present at startup will be
+     * removed during cleanup.
+     * </p>
+     *
+     * @param path the {@link Path} to the file that needs to be registered
      */
     void register(Path path);
 
     /**
-     * A positive number incremented on every transaction.
+     * Returns the current transaction commit ID.
+     * <p>
+     * This ID is incremented with each transaction and serves as a unique identifier for
+     * tracking file operations.
+     * </p>
+     *
+     * @return a positive long number representing the current commit ID
      */
     long commitID();
 
     /**
-     * Notify that we have finished with a bucket and it should be freed after the next
-     * serialization to disk.
+     * Schedules a bucket for disposal after the next disk serialization.
      *
-     * @param bucket          The bucket to free.
-     * @param createdCommitID 0 if the bucket was created before the last node restart,
-     *                        otherwise the return value of commitID() when it was created. If
-     *                        there hasn't been a commit, we can free the bucket
-     *                        immediately...
+     * @param bucket          the {@link DelayedDispose} bucket to be disposed
+     * @param createdCommitID the commit ID when the bucket was created. Use 0 for buckets
+     *                        created before the last node restart. If no commit has occurred
+     *                        since creation, the bucket can be disposed immediately
      */
-    void delayedFree(DelayedFree bucket, long createdCommitID);
+    void delayedDispose(DelayedDispose bucket, long createdCommitID);
 
     /**
-     * Get the persistent temp files directory.
+     * Retrieves the directory path for persistent temporary files.
+     *
+     * @return a {@link Path} object representing the persistent temp files directory
      */
     Path getDir();
 
+    /**
+     * Retrieves the filename generator for creating unique filenames.
+     *
+     * @return a {@link FilenameGenerator} instance used for generating unique filenames
+     */
     FilenameGenerator getGenerator();
 
 }
