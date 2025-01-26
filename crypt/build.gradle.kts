307b4ba4b91e0bcad8e7003e009c2804bcef08b4
Leumor <116955025+leumor@users.noreply.github.com>
2025-01-26 08:56:57 +0000

    Removed apache commons rng and used java.util.random.RandomGenerator instead

--- crypt/build.gradle.kts
+++ crypt/build.gradle.kts
@@ -4,7 +4,6 @@ plugins {
 
 dependencies {
     implementation("org.bouncycastle:bcprov-jdk18on:latest.release")
-    implementation("org.apache.commons:commons-rng-simple:latest.release")
 
     implementation(project(":base"))
 }
