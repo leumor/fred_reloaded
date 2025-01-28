6afa8b70345ed68333f9578db977cfa95902bf2a
Leumor <116955025+leumor@users.noreply.github.com>
2025-01-28 10:19:48 +0000

    Improved NullOutputStream class

--- support/src/main/java/hyphanet/support/io/stream/NullOutputStream.java
+++ support/src/main/java/hyphanet/support/io/stream/NullOutputStream.java
@@ -5,16 +5,30 @@ package hyphanet.support.io.stream;
 
 import java.io.OutputStream;
 
+/**
+ * An {@link OutputStream} that discards all data written to it. This stream acts as a black hole
+ * for data, similar to <code>/dev/null</code> on Unix-like systems. It is useful in situations
+ * where an {@link OutputStream} is required, but the output data is not needed or should be
+ * ignored.
+ */
 public class NullOutputStream extends OutputStream {
-    public NullOutputStream() {
-    }
+  /**
+   * {@inheritDoc}
+   *
+   * <p>This implementation does nothing, effectively discarding the byte.
+   */
+  @Override
+  public void write(int b) {
+    // Nothing to write
+  }
 
-    @Override
-    public void write(int b) {
-    }
-
-    @Override
-    public void write(byte[] buf, int off, int len) {
-    }
+  /**
+   * {@inheritDoc}
+   *
+   * <p>This implementation does nothing, effectively discarding the buffer.
+   */
+  @Override
+  public void write(byte[] buf, int off, int len) {
+    // Nothing to write
+  }
 }
-
