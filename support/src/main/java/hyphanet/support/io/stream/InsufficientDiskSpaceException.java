152affcf92d032397994dc5d91f44a13bd879ff6
Leumor <116955025+leumor@users.noreply.github.com>
2025-01-28 13:03:20 +0000

    Added JavaDoc to InsufficientDiskSpaceException class

--- support/src/main/java/hyphanet/support/io/stream/InsufficientDiskSpaceException.java
+++ support/src/main/java/hyphanet/support/io/stream/InsufficientDiskSpaceException.java
@@ -3,6 +3,11 @@ package hyphanet.support.io.stream;
 import java.io.IOException;
 import java.io.Serial;
 
+/**
+ * Signals that an operation failed because there is insufficient disk space. This exception is
+ * typically thrown when attempting to write data to a disk or storage medium and there is not
+ * enough space available to complete the operation.
+ */
 public class InsufficientDiskSpaceException extends IOException {
   @Serial private static final long serialVersionUID = 1795900904922247498L;
 }
