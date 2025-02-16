4d750a21af25c71a1322edb18d5e9a948a4aab1e
Leumor <116955025+leumor@users.noreply.github.com>
2025-02-16 11:58:37 +0000

    Add comments to NullBucket and NullBucketFactory

--- support/src/main/java/hyphanet/support/io/storage/bucket/NullBucket.java
+++ support/src/main/java/hyphanet/support/io/storage/bucket/NullBucket.java
@@ -3,104 +3,186 @@
  * http://www.gnu.org/ for further details of the GPL. */
 package hyphanet.support.io.storage.bucket;
 
-import freenet.client.async.ClientContext;
-import freenet.support.api.Bucket;
-import freenet.support.api.LockableRandomAccessBuffer;
-import freenet.support.api.RandomAccessBucket;
-
+import hyphanet.support.io.ResumeContext;
+import hyphanet.support.io.storage.rab.NullRab;
+import hyphanet.support.io.storage.rab.Rab;
+import hyphanet.support.io.stream.NullInputStream;
+import hyphanet.support.io.stream.NullOutputStream;
 import java.io.*;
 
-public class NullBucket implements Bucket, Serializable, RandomAccessBucket {
+/**
+ * A {@link Bucket} implementation that does nothing.
+ *
+ * <p>This bucket is primarily used for testing or in situations where a bucket is required but no
+ * actual storage is needed. All write operations are discarded, and read operations return empty
+ * streams.
+ *
+ * <p>This class is {@link Serializable}.
+ */
+public class NullBucket implements Bucket, Serializable, RandomAccessible {
 
-    public static final OutputStream nullOut = new NullOutputStream();
-    public static final InputStream nullIn = new NullInputStream();
-    public final long length;
-    private static final long serialVersionUID = 1L;
+  /** A static, shared {@link NullOutputStream} instance for all {@link NullBucket} writes. */
+  public static final OutputStream nullOut = new NullOutputStream();
 
-    public NullBucket() {
-        this(0);
-    }
+  /** A static, shared {@link NullInputStream} instance for all {@link NullBucket} reads. */
+  public static final InputStream nullIn = new NullInputStream();
 
-    public NullBucket(long length) {
-        this.length = length;
-    }
+  @Serial private static final long serialVersionUID = 1L;
 
-    /**
-     * Returns an OutputStream that is used to put data in this Bucket.
-     **/
-    @Override
-    public OutputStream getOutputStream() {
-        return nullOut;
-    }
+  /** Constructs a {@link NullBucket} with a default size of 0. */
+  public NullBucket() {
+    this(0);
+  }
 
-    @Override
-    public OutputStream getOutputStreamUnbuffered() {
-        return nullOut;
-    }
+  /**
+   * Constructs a {@link NullBucket} with a specified size.
+   *
+   * @param size The size of the {@link NullBucket}. This determines the size reported by {@link
+   *     #size()} and the number of null bytes returned by read operations.
+   */
+  public NullBucket(long size) {
+    this.size = size;
+  }
 
-    /**
-     * Returns an InputStream that reads data from this Bucket. If there is no data in this
-     * bucket, null is returned.
-     **/
-    @Override
-    public InputStream getInputStream() {
-        return nullIn;
-    }
+  /**
+   * Returns an {@link OutputStream} that discards all written data.
+   *
+   * @return A shared {@link NullOutputStream} instance ({@link #nullOut}).
+   */
+  @Override
+  public OutputStream getOutputStream() {
+    return nullOut;
+  }
 
-    @Override
-    public InputStream getInputStreamUnbuffered() {
-        return nullIn;
-    }
+  /**
+   * Returns an unbuffered {@link OutputStream} that discards all written data.
+   *
+   * @return A shared {@link NullOutputStream} instance ({@link #nullOut}).
+   */
+  @Override
+  public OutputStream getOutputStreamUnbuffered() {
+    return nullOut;
+  }
 
-    /**
-     * Returns the amount of data currently in this bucket.
-     **/
-    @Override
-    public long size() {
-        return length;
-    }
+  /**
+   * Returns an {@link InputStream} that provides no data or a stream of null bytes depending on the
+   * bucket's size.
+   *
+   * @return A shared {@link NullInputStream} instance ({@link #nullIn}).
+   */
+  @Override
+  public InputStream getInputStream() {
+    return nullIn;
+  }
 
-    /**
-     * Returns the name of this NullBucket.
-     */
-    @Override
-    public String getName() {
-        return "President George W. NullBucket";
-    }
+  /**
+   * Returns an unbuffered {@link InputStream} that provides no data.
+   *
+   * @return A shared {@link NullInputStream} instance ({@link #nullIn}).
+   */
+  @Override
+  public InputStream getInputStreamUnbuffered() {
+    return nullIn;
+  }
 
-    @Override
-    public boolean isReadOnly() {
-        return false;
-    }
+  /**
+   * Returns the pre-defined size of this {@link NullBucket}.
+   *
+   * @return The size specified in the constructor.
+   */
+  @Override
+  public long size() {
+    return size;
+  }
 
-    @Override
-    public void setReadOnly() {
-        // Do nothing
-    }
+  /**
+   * Returns a fixed, humorous name for this {@link NullBucket}.
+   *
+   * @return The string "President George W. NullBucket".
+   */
+  @Override
+  public String getName() {
+    return "President George W. NullBucket";
+  }
 
-    @Override
-    public void free() {
-        // Do nothing
-    }
+  /**
+   * Always returns {@code false} as {@link NullBucket} is always considered read-write.
+   *
+   * @return {@code false}
+   */
+  @Override
+  public boolean isReadOnly() {
+    return false;
+  }
 
-    @Override
-    public RandomAccessBucket createShadow() {
-        return new NullBucket();
-    }
+  /** Does nothing. {@link NullBucket} is always read-write, and this method has no effect. */
+  @Override
+  public void setReadOnly() {
+    // Do nothing
+  }
 
-    @Override
-    public void onResume(ClientContext context) {
-        // Do nothing.
-    }
+  /**
+   * Does nothing. No resources are held by a {@link NullBucket}.
+   *
+   * <p>{@inheritDoc}
+   */
+  @Override
+  public void dispose() {
+    // Do nothing
+  }
 
-    @Override
-    public void storeTo(DataOutputStream dos) throws IOException {
-        throw new UnsupportedOperationException();
-    }
+  /**
+   * Creates a new {@link NullBucket} instance as a shadow copy.
+   *
+   * @return A new {@link NullBucket} with the same size.
+   */
+  @Override
+  public RandomAccessible createShadow() {
+    return new NullBucket(size);
+  }
 
-    @Override
-    public LockableRandomAccessBuffer toRandomAccessBuffer() throws IOException {
-        return new NullRandomAccessBuffer(length);
-    }
+  /**
+   * Does nothing. No resumption actions are needed for a {@link NullBucket}.
+   *
+   * @param context The resume context (ignored).
+   */
+  @Override
+  public void onResume(ResumeContext context) {
+    // Do nothing.
+  }
+
+  /**
+   * Throws {@link UnsupportedOperationException}. {@link NullBucket} cannot be stored.
+   *
+   * @param dos The {@link DataOutputStream} (ignored).
+   * @throws UnsupportedOperationException Always.
+   */
+  @Override
+  public void storeTo(DataOutputStream dos) throws IOException {
+    throw new UnsupportedOperationException();
+  }
+
+  /**
+   * Does nothing. No resources are held by a {@link NullBucket}.
+   *
+   * <p>{@inheritDoc}
+   */
+  @Override
+  public void close() {
+    // Do nothing
+  }
+
+  /**
+   * Returns a {@link NullRab} of the same size as this {@link NullBucket}.
+   *
+   * @return A new {@link NullRab} instance.
+   * @throws IOException Never, as {@link NullRab} creation does not throw {@link IOException}.
+   */
+  @Override
+  public Rab toRandomAccessBuffer() throws IOException {
+    return new NullRab(size);
+  }
+
+  /** The pre-defined size of this {@link NullBucket}. Reads will return this many null bytes. */
+  private final long size;
 }
-
--- support/src/main/java/hyphanet/support/io/storage/bucket/NullBucketFactory.java
+++ support/src/main/java/hyphanet/support/io/storage/bucket/NullBucketFactory.java
@@ -1,15 +1,24 @@
 package hyphanet.support.io.storage.bucket;
 
-import freenet.support.api.BucketFactory;
-import freenet.support.api.RandomAccessBucket;
-
 import java.io.IOException;
 
+/**
+ * A {@link BucketFactory} that creates {@link NullBucket} instances.
+ *
+ * <p>This factory is useful for testing and situations where a bucket is needed but no actual
+ * storage is required.
+ */
 public class NullBucketFactory implements BucketFactory {
 
-    @Override
-    public RandomAccessBucket makeBucket(long size) throws IOException {
-        return new NullBucket();
-    }
-
+  /**
+   * Creates a new {@link NullBucket} instance.
+   *
+   * @param size The pre-defined size of the {@link NullBucket}.
+   * @return A new {@link NullBucket} instance.
+   * @throws IOException Never, as {@link NullBucket} creation does not throw {@link IOException}.
+   */
+  @Override
+  public RandomAccessible makeBucket(long size) throws IOException {
+    return new NullBucket(size);
+  }
 }
