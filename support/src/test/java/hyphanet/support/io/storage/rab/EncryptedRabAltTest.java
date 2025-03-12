4f291c2b1e1752f7d65ab0a4da7e9eb0ac537b42
Leumor <116955025+leumor@users.noreply.github.com>
2025-03-12 16:03:43 +0000

    EncryptedRabAltTest

---
+++ support/src/test/java/hyphanet/support/io/storage/rab/EncryptedRabAltTest.java
@@ -0,0 +1,35 @@
+package hyphanet.support.io.storage.rab;
+
+import hyphanet.crypt.key.MasterSecret;
+import hyphanet.support.io.storage.EncryptType;
+import java.io.IOException;
+import java.security.GeneralSecurityException;
+import java.security.Security;
+import org.bouncycastle.jce.provider.BouncyCastleProvider;
+
+public class EncryptedRabAltTest extends RabTestBase {
+
+  private static final EncryptType[] types = EncryptType.values();
+
+  private static final MasterSecret secret = new MasterSecret();
+  private static final int[] TEST_LIST =
+      new int[] {0, 1, 32, 64, 32768, 1024 * 1024, 1024 * 1024 + 1};
+
+  static {
+    Security.addProvider(new BouncyCastleProvider());
+  }
+
+  public EncryptedRabAltTest() {
+    super(TEST_LIST);
+  }
+
+  @Override
+  protected Rab construct(long size) throws IOException {
+    ByteArrayRab barat = new ByteArrayRab((int) (size + types[0].headerLen));
+    try {
+      return new EncryptedRab(types[0], barat, secret, true);
+    } catch (GeneralSecurityException e) {
+      throw new Error(e);
+    }
+  }
+}
